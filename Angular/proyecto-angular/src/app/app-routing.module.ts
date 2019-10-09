8ebf885bb1f6d52c874347914b89be45333ad9b0
Roberto C <3149775+Trewqa@users.noreply.github.com>
2019-10-09 10:13:29 +0200

    Mensajes.

--- Angular/proyecto-angular/src/app/app-routing.module.ts
+++ Angular/proyecto-angular/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegistroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/proyecto-angular/src/app/mensajes/mensajes.component.ts
+++ Angular/proyecto-angular/src/app/mensajes/mensajes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MensajesService } from './../mensajes.service';
 
 @Component({
   selector: 'app-mensajes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MensajesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mensajesService: MensajesService) { }
 
   ngOnInit() {
   }
