b2883c1982bbd01634c35cbdca6e7cd0603b7356
Roberto C <3149775+Trewqa@users.noreply.github.com>
2019-10-09 09:46:37 +0200

    Formulario.

--- Angular/proyecto-angular/src/app/app.module.ts
+++ Angular/proyecto-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
