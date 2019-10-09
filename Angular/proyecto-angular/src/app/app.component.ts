81ddf6ed393fc36268ce7d5fce14c759dfcb19f4
Roberto C <3149775+Trewqa@users.noreply.github.com>
2019-10-09 09:25:43 +0200

    Imports angular modules.

--- Angular/proyecto-angular/src/app/app.component.ts
+++ Angular/proyecto-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
